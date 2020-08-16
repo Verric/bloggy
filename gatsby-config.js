57b2850dfe5066c345577da03b4700ea6179f6cf
Verric <beijing-planes@hotmail.com>
2020-08-16 19:57:33 +1000

    initial screw around

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Bloggy`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Verric`,
+      summary: `...`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `Blog?`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
   },
   plugins: [
     {
