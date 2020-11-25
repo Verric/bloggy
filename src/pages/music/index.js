230f2932e42ff860f2939ef3a1e43a3c41303deb
Verric <beijing-planes@hotmail.com>
2020-11-26 00:29:57 +1100

    added music primaitve layout

--- src/pages/music/index.js
+++ src/pages/music/index.js
@@ -1,22 +1,44 @@
-import React from "react"
-import { Card } from "semantic-ui-react"
+import React, { useState } from "react"
+import { Button, Embed, Segment, Flag, Header } from "semantic-ui-react"
 import { graphql } from "gatsby"
+import data from "./data.json"
 
-import Layout from "../../components/layout"
-
-const About = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
+const Music = () => {
+  const [videoId, setVideoId] = useState(null)
   return (
-    <Layout location={location} title={siteTitle}>
-      <Card>
-        <Card.Content>Test :)</Card.Content>
-      </Card>
-    </Layout>
+    <div
+      style={{ width: "100vw", height: "100vh" }}
+      className={videoId ? "video-set" : ""}
+    >
+      <Segment.Group compact>
+        {data.map(song => (
+          <Segment
+            compact
+            size="small"
+            key={song.id}
+            onClick={() => setVideoId(song.id)}
+          >
+            <Header>
+              <Header.Content>{`${song.title} - ${song.artist}`}</Header.Content>
+              <Flag style={{ marginLeft: "5px" }} name={song.country} />
+            </Header>
+            <p>{`Released: ${song.releaseDate}`}</p>
+          </Segment>
+        ))}
+      </Segment.Group>
+      {videoId && (
+        <section style={{ maxHeight: "50%", width: "500px", height: "500px" }}>
+          <Button color="red" onClick={() => setVideoId(null)}>
+            X
+          </Button>
+          <Embed id={videoId} source="youtube" active />
+        </section>
+      )}
+    </div>
   )
 }
 
-export default About
+export default Music
 
 export const pageQuery = graphql`
   query {
