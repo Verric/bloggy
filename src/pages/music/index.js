ce46066a493360b742080cb0ee41858cd0fc72ae
Verric <beijing-planes@hotmail.com>
2020-12-21 00:15:41 +1100

    some primitive layout to music page

--- src/pages/music/index.js
+++ src/pages/music/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react"
 import { Button, Embed, Segment, Select, Header } from "semantic-ui-react"
 import data from "./data.json"
 
+import "./music.css"
+
 const OPTIONS = [
   {
     text: "Artist",
@@ -31,64 +33,72 @@ const Music = () => {
   }, [grouping])
 
   return (
-    <div
-      style={{ width: "100vw", height: "100vh" }}
-      className={videoId ? "video-set" : ""}
-    >
-      <Select
-        options={OPTIONS}
-        value={grouping}
-        defaultValue={grouping}
-        placeholder="group by"
-        onChange={(e, { value }) => {
-          e.preventDefault()
-          setGrouping(value)
-        }}
-      />
+    <div className="page-wrapper">
+      <div className="main-area">
+        <Select
+          options={OPTIONS}
+          value={grouping}
+          defaultValue={grouping}
+          placeholder="group by"
+          onChange={(e, { value }) => {
+            e.preventDefault()
+            setGrouping(value)
+          }}
+        />
 
-      {Object.values(songData).map((group, index) => (
-        <Segment.Group compact>
-          <Segment>
-            <Header>{group[0][grouping]}</Header>
-          </Segment>
-          <Segment.Group key={index} compact style={{ margin: "3px" }}>
-            {group.map(song => (
-              <Segment
-                compact
-                size="small"
-                key={song.id}
-                onClick={() => setVideoId(song.id)}
-              >
-                <Header>
-                  <Header.Content>{song.title}</Header.Content>
-                  <Header.Subheader>{`${song.title} - ${song.releaseDate}`}</Header.Subheader>
-                </Header>
-              </Segment>
-            ))}
+        {Object.values(songData).map((group, index) => (
+          <Segment.Group compact>
+            <Segment>
+              <Header>{group[0][grouping]}</Header>
+            </Segment>
+            <Segment.Group key={index} compact style={{ margin: "3px" }}>
+              {group.map(song => (
+                <Segment
+                  compact
+                  size="small"
+                  key={song.id}
+                  onClick={() => setVideoId(song.id)}
+                >
+                  <Header>
+                    <Header.Content>{song.title}</Header.Content>
+                    <Header.Subheader>{`${song.title} - ${song.releaseDate}`}</Header.Subheader>
+                  </Header>
+                </Segment>
+              ))}
+            </Segment.Group>
           </Segment.Group>
-        </Segment.Group>
-      ))}
+        ))}
+      </div>
 
-      {videoId && (
-        <section style={{ maxHeight: "50%", width: "500px", height: "500px" }}>
-          <Button color="red" onClick={() => setVideoId(null)}>
-            X
-          </Button>
-          <Embed id={videoId} source="youtube" active />
-        </section>
-      )}
+      <section className="video-area">
+        <div className="video-box">
+          {videoId ? (
+            <>
+              <Button
+                color="red"
+                onClick={() => setVideoId(null)}
+                content="X"
+              />
+              <Embed id={videoId} source="youtube" active />
+            </>
+          ) : (
+            <p
+              style={{
+                backgroundColor: "lightgrey",
+                margin: "auto",
+                textAlign: "center",
+              }}
+            >
+              Select a track
+            </p>
+          )}
+        </div>
+        <div className="song-description">
+          {songData.description || "Lorem Ipsum"}
+        </div>
+      </section>
     </div>
   )
 }
 
 export default Music
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
